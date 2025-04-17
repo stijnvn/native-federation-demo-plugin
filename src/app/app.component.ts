e703f377adb063c5cbb99b9cdd3b3f3e0296129d
Van Nieuwenhuyse, Stijn <stijn.vannieuwenhuyse@teledyne.com>
2025-04-17 09:12:47 +0200

    Set up user details plugin remote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'plugin-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'native-federation-demo-plugin';
-}
+export class AppComponent {}
