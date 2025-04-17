c31a4eac8a4254b3d10f58ec5791c0c1e9babce3
Van Nieuwenhuyse, Stijn <stijn.vannieuwenhuyse@teledyne.com>
2025-04-17 16:56:41 +0200

    Use @stijnvn/user package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {UserService} from '@stijnvn/user';
 
 @Component({
   selector: 'plugin-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly userService = inject(UserService);
+
+  user = this.userService.user;
+
+  constructor() {
+    this.userService.logIn()
+    this.userService.updateName('foobar')
+  }
+}
